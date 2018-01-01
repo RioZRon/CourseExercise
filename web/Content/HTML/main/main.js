5f3322956701ded4c9a347fa57b4ee0f3e40d33a
RioZRon <17323844268@163.com>
2018-01-01 17:22:17 +0800

    主页完成
    主页完成,但是感觉点击之后应该还有动画

---
+++ web/Content/HTML/main/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by riosysm on 2017/12/31.
+ */
+function turnblue(){
+    document.getElementById("detail").style.color="#2a96db";
+    document.getElementById("detailArr").style.marginLeft="7px";
+    document.getElementById("detailArr").src="../../../WEB-INF/res/pic/PNG/detailblue.png";
+}
+
+function turnback(){
+    document.getElementById("detail").style.color="black";
+    document.getElementById("detailArr").style.marginLeft="3px";
+    document.getElementById("detailArr").src="../../../WEB-INF/res/pic/PNG/detailB.png";
+}
