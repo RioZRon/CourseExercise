848c9d445c7d8c32d3ffd7d65097e480c7a38d00
RioZRon <17323844268@163.com>
2018-01-03 09:06:52 +0800

    准备工作完成
    后端与数据库同时写

--- web/Content/HTML/main/main.js
+++ web/Content/HTML/main/main.js
@@ -12,3 +12,62 @@ function turnback(){
     document.getElementById("detailArr").style.marginLeft="3px";
     document.getElementById("detailArr").src="../../../WEB-INF/res/pic/PNG/detailB.png";
 }
+
+function focusFirst(){
+    document.getElementById("overview").style.color="#2a96db";
+    document.getElementById("overview").style.fontSize="24px";
+    document.getElementById("overview").style.marginBottom="20px";
+    document.getElementById("overview-content").style.marginBottom="20px";
+}
+
+function blurFirst(){
+    document.getElementById("overview").style.color="black";
+    document.getElementById("overview").style.fontSize="14px";
+    document.getElementById("overview").style.marginBottom="4px";
+    document.getElementById("overview-content").style.marginBottom="";
+}
+
+function focusSecond(){
+    document.getElementById("expert").style.color="#2a96db";
+    document.getElementById("expert").style.fontSize="24px";
+    document.getElementById("expert").style.marginBottom="20px";
+    document.getElementById("expert-content").style.marginBottom="20px";
+}
+
+function blurSecond(){
+    document.getElementById("expert").style.color="black";
+    document.getElementById("expert").style.fontSize="14px";
+    document.getElementById("expert").style.marginBottom="4px";
+    document.getElementById("expert-content").style.marginBottom="";
+}
+
+function focusThird(){
+    document.getElementById("canlendar").style.color="#2a96db";
+    document.getElementById("canlendar").style.fontSize="24px";
+    document.getElementById("canlendar").style.marginBottom="20px";
+    document.getElementById("canlendar-content").style.marginBottom="20px";
+}
+
+function blurThird(){
+    document.getElementById("canlendar").style.color="black";
+    document.getElementById("canlendar").style.fontSize="14px";
+    document.getElementById("canlendar").style.marginBottom="4px";
+    document.getElementById("canlendar-content").style.marginBottom="";
+}
+
+function showhide_password(){
+    var pwd=document.getElementById("password");
+    var img=document.getElementById("showpassword")
+    if(tack == 0){
+        pwd.type="text";
+        img.src="../../res/png/function/眼.png";
+        tack =  1;
+    }
+    else
+    {
+        img.src="../../res/png/function/眼%20(1).png";
+        tack = 0;
+        pwd.type="password";
+
+    }
+}
